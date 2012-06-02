d21970b2120f84932f38d97f514911fbb0f36a4e
Mauricio Ruiz <maudiruiz@gmail.com>
2012-06-01 20:23:31 -0500

    Agregué la barra para usuario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,38 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+ 
   
+  def currentUser
+
+
+      if  current_user && !current_protectora && !current_admin
+        @currentUser = current_user
+         @creadorID = current_user.id
+         @creator = 0
+         @currentUserPass = true
+
+       elsif current_protectora && !current_admin
+         @currentUser = current_protectora
+
+         @creator = 1
+         @creadorID = current_protectora.id 
+         @currentUserPass = true
+
+       elsif current_admin 
+         @currentUser = current_admin
+
+         @creator = 2
+         @creadorID = current_admin.id
+         @currentUserPass = true
+
+       end
+
+
+       @misMascotas = Mascotum.find(:all, :conditions => {:protectora => @creadorID,:creator=>@creator})
+
+   
+
+
+   end
 
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,35 +1,4 @@
 class UserController < ApplicationController
-  def currentUser
-    
-
-     if  current_user && !current_protectora && !current_admin
-       @currentUser = current_user
-        @creadorID = current_user.id
-        @creator = 0
-        @currentUserPass = true
-        
-      elsif current_protectora && !current_admin
-        @currentUser = current_protectora
-        
-        @creator = 1
-        @creadorID = current_protectora.id 
-        @currentUserPass = true
-
-      elsif current_admin 
-        @currentUser = current_admin
-        
-        @creator = 2
-        @creadorID = current_admin.id
-        @currentUserPass = true
-        
-      end
-
-    
-      @misMascotas = Mascotum.find(:all, :conditions => {:protectora => @creadorID,:creator=>@creator})
-    
-     
-        
-      
-  end
+ 
 
 end
